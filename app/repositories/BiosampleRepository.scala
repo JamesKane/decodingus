a75cf962fb8aba9f7390243c42cd8f7557958134
jkane <jkane@tds.net>
2025-05-14 19:05:05 -0500

    Load the Publication's samples from the DB instead of a Mock
    TODO: Will need to be tested when trees are implemented.

--- app/repositories/BiosampleRepository.scala
+++ app/repositories/BiosampleRepository.scala
@@ -1,22 +1,77 @@
 package repositories
 
+import com.vividsolutions.jts.geom.Point
+import com.vividsolutions.jts.io.WKBReader
 import jakarta.inject.Inject
 import models.Biosample
+import models.api.{BiosampleWithOrigin, GeoCoord}
 import models.dal.{DatabaseSchema, MyPostgresProfile}
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
+import slick.jdbc.GetResult
 
 import scala.concurrent.{ExecutionContext, Future}
 
 trait BiosampleRepository {
   def findById(id: Int): Future[Option[Biosample]]
+  def findBiosamplesWithOriginForPublication(publicationId: Int): Future[Seq[BiosampleWithOrigin]]
 }
 
 class BiosampleRepositoryImpl @Inject()(protected val dbConfigProvider: DatabaseConfigProvider)
-                             (implicit ec: ExecutionContext) extends BiosampleRepository with HasDatabaseConfigProvider[MyPostgresProfile] {
+                                       (implicit ec: ExecutionContext) extends BiosampleRepository with HasDatabaseConfigProvider[MyPostgresProfile] {
   import models.dal.MyPostgresProfile.api.*
   private val biosamplesTable = DatabaseSchema.biosamples
+
+  private def readPoint(pgObj: AnyRef): Option[GeoCoord] = pgObj match {
+    case null => None
+    case _ =>
+      try {
+        val wkbReader = new WKBReader()
+        val point = wkbReader.read(WKBReader.hexToBytes(pgObj.toString)).asInstanceOf[Point]
+        Some(GeoCoord(point.getX, point.getY))
+      } catch {
+        case e: Exception =>
+          println(s"Error reading WKB: ${pgObj.toString} - ${e.getMessage}")
+          None
+      }
+  }
+
+  implicit val getBiosampleWithOriginResult: GetResult[BiosampleWithOrigin] = GetResult(r =>
+    BiosampleWithOrigin(
+      sampleName = r.nextStringOption(),
+      enaAccession = r.nextString(),
+      sex = r.nextStringOption(),
+      geoCoord = r.nextObjectOption().flatMap(readPoint),
+      yDnaHaplogroup = r.nextStringOption(),
+      mtDnaHaplogroup = r.nextStringOption()
+    )
+  )
 
   override def findById(id: Int): Future[Option[Biosample]] = {
     db.run(biosamplesTable.filter(_.id === id).result.headOption)
   }
+
+
+  override def findBiosamplesWithOriginForPublication(publicationId: Int): Future[Seq[BiosampleWithOrigin]] = {
+    val sqlString =
+      s"""
+         SELECT b.alias,
+                b.sample_accession,
+                b.sex,
+                b.geocoord,
+                MAX(CASE WHEN h.haplogroup_type = 'Y' THEN h.name END)  AS y_haplogroup_name,
+                MAX(CASE WHEN h.haplogroup_type = 'MT' THEN h.name END) AS mt_haplogroup_name
+         FROM publication_biosample pb
+                  INNER JOIN
+              public.biosample b ON b.id = pb.biosample_id
+                  LEFT JOIN
+              biosample_haplogroup bh ON bh.sample_guid = b.sample_guid
+                  LEFT JOIN
+              haplogroup h ON h.haplogroup_id = bh.y_haplogroup_id AND h.haplogroup_type IN ('Y', 'MT')
+         WHERE pb.publication_id = $publicationId
+         GROUP BY b.alias, b.sample_accession, b.sex, b.geocoord;
+      """
+
+    db.run(sql"#${sqlString}".as[BiosampleWithOrigin])
+  }
+
 }
--- app/services/BiosampleReportService.scala
+++ app/services/BiosampleReportService.scala
@@ -1,21 +1,12 @@
 package services
 
-import models.api.{BiosampleWithOrigin, GeoCoord}
+import models.api.BiosampleWithOrigin
+import repositories.BiosampleRepository
 
-import javax.inject.{Inject, Singleton}
+import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
-@Singleton
-class BiosampleReportService @Inject()(implicit ec: ExecutionContext) {
-  def getBiosampleData(publicationId: Int): Future[Seq[BiosampleWithOrigin]] = Future {
-    // Replace this with your actual data fetching from your internal system
-    // This is just dummy data for demonstration
-    val foo = GeoCoord(45.0, 90.0)
-    publicationId match {
-      case 1 => Seq(BiosampleWithOrigin("foo", "ERS123456", "H1", "tbd", Some(foo)), 
-        BiosampleWithOrigin("bar", "ERS789012", "R2", "tbd", None))
-      case 456 => Seq(BiosampleWithOrigin("baz", "DRS987654", "J1c", "tbd", Some(foo)))
-      case _ => Seq.empty
-    }
-  }
+class BiosampleReportService @Inject(biosampleRepository: BiosampleRepository)(implicit ec: ExecutionContext) {
+  def getBiosampleData(publicationId: Int): Future[Seq[BiosampleWithOrigin]] =
+    biosampleRepository.findBiosamplesWithOriginForPublication(publicationId)
 }
