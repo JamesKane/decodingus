1c5a47dd29a28f70d01b10f5b015f0a13ba607aa
jkane <jkane@tds.net>
2025-05-17 16:34:04 -0500

    Add detailed Scaladoc comments to HaplogroupCoreRepository

--- app/repositories/HaplogroupCoreRepository.scala
+++ app/repositories/HaplogroupCoreRepository.scala
@@ -6,9 +6,33 @@ import play.api.db.slick.DatabaseConfigProvider
 
 import scala.concurrent.{ExecutionContext, Future}
 
+/**
+ * Repository interface for accessing and managing haplogroup data.
+ */
 trait HaplogroupCoreRepository {
+  /**
+   * Retrieves a haplogroup by its name and type.
+   *
+   * @param name           the name of the haplogroup to retrieve
+   * @param haplogroupType the type of haplogroup (e.g., Y or MT)
+   * @return a Future containing an Option wrapping the Haplogroup object if found, or None otherwise
+   */
   def getHaplogroupByName(name: String, haplogroupType: HaplogroupType): Future[Option[Haplogroup]]
+
+  /**
+   * Retrieves the ancestor haplogroups of the specified haplogroup.
+   *
+   * @param haplogroupId the unique identifier of the haplogroup for which ancestors are to be retrieved
+   * @return a Future containing a sequence of ancestor Haplogroup objects
+   */
   def getAncestors(haplogroupId: Int): Future[Seq[Haplogroup]]
+
+  /**
+   * Retrieves the direct children of a given haplogroup by its unique identifier.
+   *
+   * @param haplogroupId the unique identifier of the haplogroup whose direct children are to be retrieved
+   * @return a Future containing a sequence of Haplogroup objects representing the direct children
+   */
   def getDirectChildren(haplogroupId: Int): Future[Seq[Haplogroup]]
 }
 
@@ -18,8 +42,7 @@ class HaplogroupCoreRepositoryImpl @Inject()(
   extends BaseRepository(dbConfigProvider)
     with HaplogroupCoreRepository {
 
-  import models.dal.DatabaseSchema.haplogroups
-  import models.dal.DatabaseSchema.haplogroupRelationships
+  import models.dal.DatabaseSchema.{haplogroupRelationships, haplogroups}
   import models.dal.MyPostgresProfile.api.*
 
   override def getHaplogroupByName(name: String, haplogroupType: HaplogroupType): Future[Option[Haplogroup]] = {
