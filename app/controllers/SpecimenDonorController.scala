8344cf061cb570ef339bd3c71ad3851473408a56
jkane <jkane@tds.net>
2025-06-07 08:55:53 -0500

    Add secure donor merge endpoint in `SpecimenDonorController`
    - Introduced `/api/private/donors/merge` POST route for merging specimen donors.
    - Incorporated `ApiSecurityAction` to secure the merge donor API.

--- app/controllers/SpecimenDonorController.scala
+++ app/controllers/SpecimenDonorController.scala
@@ -1,25 +1,29 @@
 package controllers
 
+import actions.ApiSecurityAction
 import jakarta.inject.{Inject, Singleton}
 import models.api.genomics.SpecimenDonorMergeRequest
+import play.api.libs.json.Json
 import play.api.mvc.*
 import services.genomics.SpecimenDonorService
-import play.api.libs.json.Json
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 
 @Singleton
 class SpecimenDonorController @Inject()(
                                          donorService: SpecimenDonorService,
+                                         secureApi: ApiSecurityAction,
                                          cc: ControllerComponents
                                        )(implicit ec: ExecutionContext) extends AbstractController(cc) {
 
   def mergeDonors(): Action[SpecimenDonorMergeRequest] = Action.async(parse.json[SpecimenDonorMergeRequest]) { request =>
-    donorService.mergeDonors(request.body).map { result =>
-      Ok(Json.toJson(result))
-    }.recover {
-      case e: IllegalArgumentException => BadRequest(Json.obj("error" -> e.getMessage))
-      case e: Exception => InternalServerError(Json.obj("error" -> "Failed to merge donors"))
-    }
+    secureApi.invokeBlock(request, { secureRequest =>
+      donorService.mergeDonors(request.body).map { result =>
+        Ok(Json.toJson(result))
+      }.recover {
+        case e: IllegalArgumentException => BadRequest(Json.obj("error" -> e.getMessage))
+        case e: Exception => InternalServerError(Json.obj("error" -> "Failed to merge donors"))
+      }
+    })
   }
 }
